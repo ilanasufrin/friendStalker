f31813f9211edc77f802f435a12cb157c866aa7f
Ilana Sufrin <ilanasufrin@gmail.com>
2014-07-17 19:45:24 -0400

    adds User and Friend models

---
+++ db/migrate/20140717234449_create_friends.rb
@@ -0,0 +1,14 @@
+class CreateFriends < ActiveRecord::Migration
+  def change
+    create_table :friends do |t|
+      t.string :name
+      t.string :location
+      t.float :latitude
+      t.float :longitude
+      t.integer :facebook_id
+      t.integer :twitter_id
+
+      t.timestamps
+    end
+  end
+end
