f78086241b65b60dcc2e46ff594da8c87efe0e73
kamoh <ben.serviss@gmail.com>
2014-07-20 14:30:56 -0400

    Adds seed data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Objects for tests:
+
+bob = Friend.create(
+  name: "Bob Testman", 
+  location: "Burger King", 
+  latitude: 123, 
+  longitude: 456, 
+  facebook_id: 1,
+  twitter_id: 1)
+
+zambezia = Friend.create(
+  name: "Zambezia Holgrauf", 
+  location: "Hamburger County", 
+  latitude: 777, 
+  longitude: 999, 
+  facebook_id: 2,
+  twitter_id: 2)
+
+florp = User.create(
+  name: "Florp the Alien",
+  location: "Florptown Stadium",
+  latitude: 1212121,
+  longitude: 54545454,
+  facebook_id: 11111,
+  twitter_id: 22222,
+  phone: "131-444-0258"
+  )
+
+beef = User.create(
+  name: "Beef Chesterton",
+  location: "Pinkberry",
+  latitude: 987654321,
+  longitude: 123456789,
+  facebook_id: 987987987,
+  twitter_id: 358358358,
+  phone: "111-000-4442"
+  )
+
+
