e70a0207fc1973ec18e912dd1b128576c886484d
kamoh <ben.serviss@gmail.com>
2014-07-24 20:04:22 -0400

    Fixes up nav bar partial

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   get '/recent', to: "users#recent"
 
+  get '/friends', to: "users#friends"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
