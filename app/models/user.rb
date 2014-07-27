e12c13f05db9b3d0f6d8534da10779e47a2f886b
Ilana Sufrin <ilanasufrin@gmail.com>
2014-07-27 14:37:38 -0400

    twilio can send text messages to me

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,15 @@ class User < ActiveRecord::Base
       )
   end
 
+  def notify
+    account_sid = 'AC4bb6fa4568260df672cbdb2389684bec'
+  auth_token = '97ea4ee4c5bfb09c0d3e2bfb36f0e4ef'
+  # @client = Twilio::REST::Client.new account_sid, auth_token
+  @client = Twilio::REST::Client.new account_sid, auth_token
+  message = @client.account.messages.create(:body => "Test text",
+  :to => "+14125088025",
+  :from => "+14129064747")
+  end
+
+
 end
