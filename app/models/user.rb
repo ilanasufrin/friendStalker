62ce09b016311931fba2c63c981375f97ae6e595
Ilana Sufrin <ilanasufrin@gmail.com>
2014-07-27 14:47:33 -0400

    twilio can now take arguments

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,18 @@ class User < ActiveRecord::Base
       )
   end
 
-  def notify
+  # Ben's number : "+15038942566"
+  # Ilana's number : "+14125088025"
+  # Emily's number : "+18456416151"
+
+  def notify(to, body)
     account_sid = 'AC4bb6fa4568260df672cbdb2389684bec'
   auth_token = '97ea4ee4c5bfb09c0d3e2bfb36f0e4ef'
-  # @client = Twilio::REST::Client.new account_sid, auth_token
   @client = Twilio::REST::Client.new account_sid, auth_token
-  message = @client.account.messages.create(:body => "Test text",
-  :to => "+14125088025",
-  :from => "+14129064747")
+  message = @client.account.messages.create(
+    :body => "#{body}",
+    :to => "#{to}",
+    :from => "+14129064747")
   end
 
 
