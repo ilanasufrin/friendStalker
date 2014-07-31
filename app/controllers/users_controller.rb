d4687c05bbffda6414fd687a02e69d2233fe9e4c
Emily Lowing <elowing@gmail.com>
2014-07-30 21:10:15 -0400

    fixes merge conflicts, restores user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,8 @@
 class UsersController < ApplicationController
   skip_before_action :authentication_required, :only => [:index]
 
-  def show
-    # raise params.inspect
-    @user = User.find(params[:id])
-    @friends = @user.friends
-  end
-  
-
   private
     def set_user
       @user = User.find(params[:id])
     end
-
-   
-    
-end
-
-
-=======
 end
