7d98eb4ba791bdf60e8a6746e700b7d6f8a7ff27
ilanasufrin <ilanasufrin@gmail.com>
2014-07-30 21:45:08 -0400

    trying to get edit params working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,24 @@ class UsersController < ApplicationController
   before_action :set_user, :only => :edit
 
   def edit
-  #  @user = User.update
   end
 
+  def update
+  @user = User.find(params[:id])
+  if @user.update(update_user_location)
+    redirect_to(@user)
+  else
+    render :edit
+  end
+  end
 
   private
     def set_user
       @user = User.find(params[:id])
     end
+
+    def update_user_location
+      params.require(:user).permit([:location])
+    end
+
 end
