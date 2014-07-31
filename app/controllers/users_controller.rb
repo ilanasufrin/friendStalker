15e47a137894c1617dd9279a017ad76c020a7dd3
ilanasufrin <ilanasufrin@gmail.com>
2014-07-30 21:20:48 -0400

    adds user edit page to set location

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   skip_before_action :authentication_required, :only => [:index]
+  before_action :set_user, :only => :edit
+
+  def edit
+  #  @user = User.update
+  end
+
 
   private
     def set_user
